b5ddd5f21daa8dc58f7b7c439ea008e511ebe0a5
plsfarm <plsfarm@proton.me>
2024-11-25 22:43:46 +0100

    global getters

--- src/pages/presale/raffle.js
+++ src/pages/presale/raffle.js
@@ -7,23 +7,6 @@ const RAFFLE_ADDRESS = '0x2E654Fa56fBb77473C9D577126163dD5F04d5d8A';
 const web3 = new Web3(window.ethereum);
 const contract = new web3.eth.Contract(ABI_REGISTER, RAFFLE_ADDRESS);
 
-// Get time left until raffle ends
-export const getTimeLeft = async () => {
-  try {
-    const endTime = await contract.methods.endTime().call();
-    const now = Math.floor(Date.now() / 1000); // Current timestamp in seconds
-
-    if (endTime < now) {
-      return new BigNumber(0);
-    }
-
-    const timeLeft = new BigNumber(endTime).minus(now);
-    return timeLeft;
-  } catch (error) {
-    console.error("Error fetching time left: ", error);
-    throw error;
-  }
-};
 
 // Get the number of tickets owned by the current account
 export const getTickets = async () => {
@@ -72,20 +55,5 @@ export const raffle = async (_tickets) => {
       success: false,
       error: error.message,
     };
-  }
-};
-
-// Fetch all registered participants
-export const getRegisteredUsers = async () => {
-  try {
-    const [addresses, ticketCounts] = await contract.methods.getRegisteredParticipants().call();
-    const participants = addresses.map((address, index) => ({
-      address,
-      tickets: new BigNumber(ticketCounts[index]),
-    }));
-    return participants;
-  } catch (error) {
-    console.error("Error fetching registered users: ", error);
-    throw error;
   }
 };
